302be2fc735b711cde22a17a7e56864aa96b1e8a
eslam <eslam@org.com>
2023-10-19 14:26:33 -0400

    mm

--- execute.c
+++ execute.c
@@ -1,58 +1,106 @@
 #include "monty.h"
-
 /**
- *execute - executes the opcode
- *@stack: head linked list - stack
- *@counter: line_counter
- *@file: poiner to monty file
- *@content: line content
- *Return: no return
+ * execute_file - Executes the operations in a file
+ * @file_name: Name of the file containing the operations
+ *
+ * Return: 0 on success, -1 on failure
  */
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+int execute_file(char *file_name)
 {
-	instruction_t opst[] = {
-		{ "push", f_push
-		},
-		{ "pall", f_pall
-		},
-		{ "pint", f_pint
-		},
-		{ "pop", f_pop
-		},
-		{ "swap", f_swap
-		},
-		{ "add", f_add
-		},
-		{ "div", f_div
-		},
-	};
-
-	unsigned int i = 0;
-	char *op;
-
-	op = strtok(content, " \n\t");
-	if (op && op[0] == '#')
-		return (0);
-	bus.arg = strtok(NULL, " \n\t");
-	while (opst[i].opcode && op)
+	FILE *file;
+	char *line = NULL;
+	size_t line_length = 0;
+	ssize_t read;
+	/*Open the file*/
+	file = fopen(file_name, "r");
+	if (file == NULL)
+		print_open_file_error(file_name);
+	my_data.file = file;
+	/*Read and execute each line until the end of the file*/
+	while ((read = getline(&line, &line_length, file)) != -1)
 	{
-		if (strcmp(op, opst[i].opcode) == 0)
+		my_data.line_number++;
+		my_data.line = line;
+		if (is_empty_line(my_data.line) == 1)
+			execute_line(my_data.line, my_data.line_number);
+	}
+	free_stack(my_data.stack);
+	fclose(file);
+	free(line);
+	return (0);
+}
+/**
+ * execute_line - Executes a single line of operation
+ * @line: Line of operation
+ * @line_number: Line number of the current operation
+ */
+void execute_line(char *line, int line_number)
+{
+	int i;
+	int is_instruction = -1;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pop", pop},
+		{"pall", pall},
+		{"pint", pint},
+		{"swap", swap},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", _div},
+		{"add", add},
+		{"mul", mul},
+		{"mod", mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{"stack", stack},
+		{"queue", queue},
+	};
+	my_data.arg1 = strtok(line, " \n");
+	my_data.arg2 = strtok(NULL, " \n");
+	if (my_data.arg1[0] == '#')
+	{
+		nop(&my_data.stack, line_number);
+		return;
+	}
+	for (i = 0; i < INSTRUCTIONS_COUNT; i++)
+		if (strcmp(my_data.arg1, instructions[i].opcode) == 0)
 		{
-			opst[i].f(stack, counter);
+
+			is_instruction = i;
+			break;
+		}
+	if (is_instruction != -1)
+		instructions[i].f(&my_data.stack, line_number);
+	else
+		print_invalid_instruction_error(line_number, my_data.arg1);
+}
+/**
+ * is_empty_line - .
+ * @line: Line to check
+ *
+ * Return: 1 if the line is empty, 0 otherwise
+ */
+int is_empty_line(char *line)
+{
+	int i;
+	int length = strlen(line);
+
+	if (length > 0)
+	{
+		/* Check if the line ends with '\n'*/
+		if (line[length - 1] == '\n')
+		{
+			/*Check if the line contains any characters before '\n'*/
+			for (i = 0; i < length - 1; i++)
+			{
+				if (line[i] != ' ')
+					return (1);
+			}
 			return (0);
 		}
-
-		i++;
 	}
 
-	if (op && opst[i].opcode == NULL)
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
-		fclose(file);
-		free(content);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-
-	return (1);
+	return (0);
 }
--- monty.c
+++ monty.c
@@ -1,45 +1,20 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
+
+my_data_t my_data = {0, NULL, NULL, NULL, NULL, NULL, STACK};
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
+ * main - Entry point for the Monty interpreter program
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int main(int argc, char **argv)
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+		print_usage_error();
+
+
+	return (execute_file(argv[1]));
+
 }
